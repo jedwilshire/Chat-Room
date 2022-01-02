a0ac63fd65c0c64bcd859fd10b8e8dd78e8406a0
Jed Wilshire <jedwilshire@gmail.com>
2022-01-02 09:31:46 -0600

    Create chat_server_2.py

---
+++ chat_server_2.py
@@ -0,0 +1,34 @@
+from socket import socket, gethostname, gethostbyname
+from network_tools import get_ip
+print('Creating Chat Server')
+
+# the socket function returns a new socket object
+chat_socket = socket()
+# gethostname function returns the name of this machine
+host_name = gethostname()
+# gethostbyname function returns the ip address of machine with given name
+# ip = gethostbyname(host_name) # this line of code does not work on linux machines
+ip = get_ip() # workaround to get ip for linux
+# we pick a random port in the so called safe range 1024-65535
+port = 8080
+chat_socket.bind( (ip, port) )
+print('New socket binding succcessful!')
+print('Your IP Address is:', ip)
+print('Your name is:', host_name)
+
+chat_socket.listen(1)
+connection, address = chat_socket.accept()
+print('Connected to', address[0])
+
+client = connection.recv(1024).decode()
+print(client, 'has connected.')
+# send first message : our name
+connection.send(host_name.encode())
+client_message = ''
+host_message = ''
+while client_message != 'QUIT' and host_message != 'QUIT':
+    server_message = input('>>> ')
+    connection.send(server_message.encode())
+    client_message = connection.recv(1024).decode()
+    print(client, ':', client_message)
+chat_socket.close()
